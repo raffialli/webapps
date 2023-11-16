3a883fa715a575b9de01c1129470dd63e533b098
Raffi Alli <Raffialli@gmail.com>
2023-11-16 03:26:13 -0500

    Adding error handling

--- app.py
+++ app.py
@@ -1,39 +1,45 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, flash, redirect, url_for
 import os
+from werkzeug.utils import secure_filename
 
-#testing Git...need a file change
+# Allowed file extensions and maximum file size
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+MAX_FILE_SIZE = 16 * 1024 * 1024  # 16 MB
+
 app = Flask(__name__)
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'super secret key'  # Needed for flashing messages
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['MAX_CONTENT_LENGTH'] = MAX_FILE_SIZE
 
 # Ensure the upload folder exists
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
+        # Check if the post request has the file part
         if 'file' not in request.files:
-            return 'No file part'
+            flash('No file part')
+            return redirect(request.url)
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
-            return 'No selected file'
-        if file:
-            filename = file.filename
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return f'File {filename} uploaded successfully.'
+            flash(f'File {filename} uploaded successfully.')
+            return redirect(url_for('upload_file'))
+        else:
+            flash('File type not allowed')
+            return redirect(request.url)
 
-    return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-          <input type=file name=file>
-          <input type=submit value=Upload>
-        </form>
-        '''
+    return render_template('upload.html')
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
