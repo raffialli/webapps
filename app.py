44bfde540672d9313841f45a8f23433f1be60660
Raffi Alli <Raffialli@gmail.com>
2023-11-16 14:28:17 -0500

    syncing error handeling changes

--- app.py
+++ app.py
@@ -1,32 +1,70 @@
 from flask import Flask, render_template, request, send_from_directory, flash, redirect, url_for
-import os
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
+import os
 
 # Allowed file extensions and maximum file size
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 MAX_FILE_SIZE = 16 * 1024 * 1024  # 16 MB
 
 app = Flask(__name__)
-app.secret_key = 'super secret key'  # Needed for flashing messages
+app.secret_key = 'super secret key'  # Needed for flashing messages and Flask-Login
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['MAX_CONTENT_LENGTH'] = MAX_FILE_SIZE
+
+# Setup Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 # Ensure the upload folder exists
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
+# User model
+class User(UserMixin):
+    id = 1
+    username = 'my'
+    password_hash = generate_password_hash('B@ll5!tch')
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# User loader
+@login_manager.user_loader
+def load_user(user_id):
+    return User() if int(user_id) == 1 else None
+
+# Helper function to check allowed file types
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Routes
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == User().username and User().check_password(password):
+            login_user(User())
+            return redirect(url_for('upload_file'))
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 @app.route('/', methods=['GET', 'POST'])
+@login_required
 def upload_file():
     if request.method == 'POST':
-        # Check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
@@ -42,10 +80,12 @@ def upload_file():
     return render_template('upload.html')
 
 @app.route('/uploads/<filename>')
+@login_required
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/files')
+@login_required
 def list_files():
     files = os.listdir(app.config['UPLOAD_FOLDER'])
     return render_template('files.html', files=files)
