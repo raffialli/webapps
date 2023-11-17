d006b7559cea22d277d2310ee26408bd0249c8f0
Libalah! Libalah! Timmy!! <89951997+raffialli@users.noreply.github.com>
2023-11-17 09:14:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 # User model
 class User(UserMixin):
     id = 1
-    username = 'yeah'
-    password_hash = generate_password_hash('sure')
+    username = 'my'
+    password_hash = generate_password_hash('B@ll5!tch')
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
